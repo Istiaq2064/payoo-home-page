e4a11e160718072923876c2413ef000151c8bdf6
Istiaq2064 <arifistiaqcse@gmail.com>
2024-09-20 16:16:25 +0600

    added add money in display money

--- js/home.js
+++ js/home.js
@@ -1,8 +1,20 @@
-document.getElementById('btn-add-mpney')
+document.getElementById('btn-add-money')
     .addEventListener('click',function(event){
         event.preventDefault();
     const addAmount = document.getElementById('input-amount').value;
     console.log(addAmount);
     const addPin = document.getElementById('input-pin-number').value;
     console.log(addPin);
+    if(addPin === '12345'){
+        const balance = document.getElementById('Account-balance').innerText;
+        console.log(balance);
+        const addMoneyNumber =parseFloat(addAmount);
+        const balanceNumber= parseFloat(balance);
+        const newBalance =  balanceNumber + addMoneyNumber;
+        document.getElementById('Account-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('wrong pin.please try again')
+    }
     });
