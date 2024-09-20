d8fd51bd8bdd9dff93c905df4ae0def3f3d59307
Istiaq2064 <arifistiaqcse@gmail.com>
2024-09-20 15:53:48 +0600

    add a add money form

---
+++ js/home.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-add-mpney')
+    .addEventListener('click',function(event){
+        event.preventDefault();
+    const addAmount = document.getElementById('input-amount').value;
+    console.log(addAmount);
+    const addPin = document.getElementById('input-pin-number').value;
+    console.log(addPin);
+    });
